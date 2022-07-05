55b06e2e6dad5be5133155bd7f95e9bb79b7f140
meta-eklavya-joshi <108259094+meta-eklavya-joshi@users.noreply.github.com>
2022-07-05 13:16:57 +0530

    Update StringOperations.java

--- StringOperations.java
+++ StringOperations.java
@@ -1,112 +1,26 @@
-//import java.util.Scanner;
-//
-//public class StringOperations {
-//	
-//	public int compareStrings(char[] str1, char[] str2)
-//	{
-//		if (str1.length != str2.length)
-//		{
-//			return 0;
-//		}
-//		else
-//		{
-//			for(int i=0; i < str1.length; i++)
-//			{
-//				if(str1[i] != str2[i])
-//				{
-//					return 0;
-//				}
-//			}
-//			return 1;
-//		}
-//	}
-//	
-//	
-//	public char[] reverseString(char[] str)
-//	{
-//		int str_length = str.length;
-//		char[] reverse_str = new char[str.length];
-//		for(int i=str_length-1; i>= 0; i++)
-//		{
-//			reverse_str[str_length - i] = str[i];
-//		}
-//		return reverse_str;
-//	}
-//	
-//	
-//	
-//	public char[] replaceCharacters(char[] str)
-//	{
-//		for(int i=0; i<str.length; i++)
-//		{
-//			if (96 < (int)str[i] && (int)str[i] < 123)
-//			{
-//				str[i] = (char) ((int) str[i] - 32);
-//			}
-//			else if (64 < (int)str[i] && (int)str[i] < 91)
-//			{
-//				str[i] = (char) ((int) str[i] + 32);
-//			}
-//		}
-//		return str;
-//	}
-//	
-//	
-//	public void largestWord(char[] str)
-//	{
-//		 char[] largestword= new char[30];
-//	        int initalPos=0, maxLen=0, tempLen=0, tempPos=0; 
-//	        for (int i = 0; i < str.length; i++) {
-//	            if(str[i]==' '){
-//	                tempPos = i+1;
-//	                tempLen=0;
-//	            }
-//	            else{
-//	                tempLen++;
-//	                if(maxLen<=tempLen){
-//	                    maxLen=tempLen;
-//	                    initalPos = tempPos;
-//	                }
-//	            }
-//	        }
-//	        for (int i = initalPos; i <=maxLen+initalPos ; i++) {
-//	            largestword[i-initalPos] = str[i];
-//	        }
-//	        System.out.println("Largest word is: " );
-//	        for(int i=0; i<largestword.length; i++)
-//	        	System.out.print(largestword[i]);
-//	        System.out.print("\n");
-////	        return largestword;
-//	}
-//	
-//	
-//	public static void main(String[] args)
-//	{
-//		StringOperations obj = new StringOperations();
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.println("Enter two strings for comparison: ");
-//		char[] str1 = scanner.nextLine().toCharArray();
-//		char[] str2 = scanner.nextLine().toCharArray();
-//		System.out.println(obj.compareStrings(str1, str2));
-//		System.out.println("Enter a string for largestWord: ");
-//		obj.largestWord(scanner.nextLine().toCharArray());
-//		System.out.println("Enter a string for replaceCharacter operation: ");
-//		System.out.println("After replacing the characters string is: " + obj.replaceCharacters(scanner.nextLine().toCharArray()));
-//		System.out.println("Enter a string for replaceCharacter operation: ");
-//		System.out.println("After reversing the string: " + obj.reverseString(scanner.nextLine().toCharArray()));
-//	}
-//}
-
-
-public class StringOperations {
+package Assignment2;
+public class StringOperation {
     char[] text = new char[30];
-    StringOperations(char[] str){
+    StringOperation(char[] str){
         text = str;
     }
-    void display(){
+    
+    /**
+     * This method displays the text
+     */
+    void display()
+    {
         System.out.println(text);
     }
-    int compare(char[]text1, char[] text2){
+    
+    /**
+     * This method compares the two strings
+     * @param text1
+     * @param text2
+     * @return 1 if the string 1 is equal to the string 2 and 0 if they are not
+     */
+    int compare(char[]text1, char[] text2)
+    { 
         if(text1.length == text2.length){
             for (int i = 0; i < text2.length; i++) {
                 if(text1[i] != text2[i]){
@@ -119,7 +33,14 @@ public class StringOperations {
             return 0;
         }
     }
-    char[] reverse(){
+    
+    
+    /**
+     * This method gives the reverse of the input string
+     * @return reverse of the string
+     */
+    char[] reverse()
+    {
         char[] rev = new char[30];
         for (int i = 0; i < text.length; i++) {
             rev[i] = text[text.length-i-1];
@@ -127,7 +48,13 @@ public class StringOperations {
         return rev;
     }
 
-    char[] changeCase(){
+    
+    /**
+     * This method changes the case of the letter
+     * @return string with the case changed
+     */
+    char[] changeCase()
+    {
         char[] changed = new char[30];
         changed = text;
         for (int i = 0; i < text.length; i++) {
@@ -144,7 +71,13 @@ public class StringOperations {
         return changed;
     }
 
-    char[] largestLastWord(){
+    
+    /**
+     * This method return the largest word of the string
+     * @return largest word in the string
+     */
+    char[] largestLastWord()
+    {
         char[] largestWord= new char[30];
         int initalPos=0, maxLen=0,tempLen=0,tempPos=0; 
         for (int i = 0; i < text.length; i++) {
@@ -160,21 +93,9 @@ public class StringOperations {
                 }
             }
         }
-        for (int i = initalPos; i <=maxLen+initalPos ; i++) {
+        for (int i = initalPos; i <=maxLen+initalPos && i < text.length; i++) {
             largestWord[i-initalPos] = text[i];
         }
         return largestWord;
-    }
-    public static void main(String[] args) {
-        char[] text1 = {'a',' ','a','n',' ','a','n','u','a','a','n','u','r',' ','a',' ','a','n','u',' ','a','n','y','r',' ','a'};
-        char[] text2 = {'a','b','c','d'};
-        // char[] text1 = {'a','B','C','d','e'};
-        StringOperations a = new StringOperations(text1);
-
-        a.display();
-        System.out.println(a.compare(text1, text2));
-        System.out.println(a.reverse());
-        System.out.println(a.changeCase());
-        System.out.println(a.largestLastWord());
     }
 }
