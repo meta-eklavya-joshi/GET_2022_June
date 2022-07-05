47ca276b456958a98f8544036e4c3b6423dce55c
meta-eklavya-joshi <108259094+meta-eklavya-joshi@users.noreply.github.com>
2022-07-05 13:14:31 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,49 +1,47 @@
-import java.util.Scanner;
-
+package Assignment2;
 public class Area {
 
+	/**
+	 * 
+	 * @param width Width of the triangle for finding area
+	 * @param height Height of the triangle for finding area
+	 * @return Area of the triangle
+	 */
 	public double areaOfTriangle(double width, double height)
 	{
 		return (double)(width * height)/2;
 	}
 	
-	
+	/**
+	 * 
+	 * @param width Width of the rectangle for finding area
+	 * @param height Height of the rectangle for finding area
+	 * @return Area of the rectangle
+	 */
 	public double areaOfRectangle(double width, double height)
 	{
 		return (double)(height * width);
 	}
 	
 	
-	public double areaOfSquare(double width)
+	/**
+	 * 
+	 * @param width Width of the square for finding area
+	 * @return Area of the square
+	 */
+	public double areaOfSquare(double width)  
 	{
 		return (double)(width*width);
 	}
 	
 	
-	public double areaOfCircle(double radius)
+	/**
+	 * 
+	 * @param width Width of the circle for finding area
+	 * @return Area of the circle
+	 */
+	public double areaOfCircle(double radius)  
 	{
 		return (double)(3.14 * radius * radius);
 	}
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Area obj = new Area();
-		System.out.println("Enter width of the Triangle: ");
-		double widthofTriangle = scanner.nextDouble();
-		System.out.println("Enter height of the Triangle: ");
-		double heightofTriangle = scanner.nextDouble();
-		System.out.println("Area of Triangle: " + obj.areaOfTriangle(widthofTriangle,heightofTriangle));
-		System.out.println("Enter width of the Rectangle: ");
-		double widthofRectangle = scanner.nextDouble();
-		System.out.println("Enter height of the Rectangle: ");
-		double heightofRectangle = scanner.nextDouble();
-		System.out.println("Area of Rectangle: " + obj.areaOfRectangle(widthofRectangle,heightofRectangle));
-		System.out.println("Enter width of the Square: ");
-		double widthofSquare = scanner.nextDouble();
-		System.out.println("Area of Square: " + obj.areaOfSquare(widthofSquare));
-		System.out.println("Enter radius of the Circle: ");
-		double radiusofCircle = scanner.nextDouble();
-		System.out.println("Area of Circle: " + obj.areaOfCircle(radiusofCircle));
-	}
-
 }
