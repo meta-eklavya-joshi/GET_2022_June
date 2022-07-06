09b81c6f10093f6e8cbef41068ba3058616e4149
meta-eklavya-joshi <108259094+meta-eklavya-joshi@users.noreply.github.com>
2022-07-06 11:49:56 +0530

    Update BaseCalc.java

--- BaseCalc.java
+++ BaseCalc.java
@@ -1,10 +1,18 @@
-
+package Assignment1;
 public class BaseCalc {
 	
 	String table = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
+	/**
+	 * This method decimal number to any base
+	 * @param num Decimal number to which base needed
+	 * @param base Base in which the decimal needs to be converted
+	 * @return String base converted number
+	 */
 	public String decimalToBase(int num, int base)
 	{
+		if (base > 36)
+			throw new AssertionError("The base cannot be greater than 36");
 		if (num == 0)
 		{
 			return "0";
@@ -26,8 +34,16 @@ public class BaseCalc {
 		return baseNum;
 	}
 	
+	/**
+	 * This method return decimal number from any base
+	 * @param str Base String to which decimal conversion is needed
+	 * @param base Base of the String number
+	 * @return Decimal number converted
+	 */
 	public int baseToDecimal(String str, int base)
 	{
+		if (base > 36)
+			throw new AssertionError("The base cannot be greater than 36");
 		int decimal = 0;
 		int power = 0;
 		int n = 0;
@@ -43,35 +59,78 @@ public class BaseCalc {
 		return decimal;
 	}
 
+	/**
+	 * This method return the base number after adding two numbers in any base
+	 * @param str1 The first number in any base
+	 * @param str2 The second number in any base
+	 * @param base The base in which the input strings are
+	 * @return String after adding the numbers in base
+	 */
 	public String add(String str1, String str2, int base)
 	{
+		if (base > 36)
+			throw new AssertionError("The base cannot be greater than 36");
 		int num1 = baseToDecimal(str1, base);
 		int num2 = baseToDecimal(str2, base);
 		return decimalToBase(num1 + num2, base);
 	}
 	
+	/**
+	 * This method return the base number after subtracting two numbers in any base
+	 * @param str1 The first number in any base
+	 * @param str2 The second number in any base
+	 * @param base The base in which the input strings are
+	 * @return String after subtracting the numbers in base
+	 */
 	public String subtract(String str1, String str2, int base)
 	{
+		if (base > 36)
+			throw new AssertionError("The base cannot be greater than 36");
 		int num1 = baseToDecimal(str1, base);
 		int num2 = baseToDecimal(str2, base);
 		return decimalToBase(num1 - num2, base);
 	}
 	
+	/**
+	 * This method return the base number after multiplying two numbers in any base
+	 * @param str1 The first number in any base
+	 * @param str2 The second number in any base
+	 * @param base The base in which the input strings are
+	 * @return String after multiplying the numbers in base
+	 */
 	public String multiply(String str1, String str2, int base)
 	{
+		if (base > 36)
+			throw new AssertionError("The base cannot be greater than 36");
 		int num1 = baseToDecimal(str1, base);
 		int num2 = baseToDecimal(str2, base);
 		return decimalToBase(num1 * num2, base);
 	}
 	
+	/**
+	 * This method return the base number after dividing two numbers in any base
+	 * @param str1 The first number in any base
+	 * @param str2 The second number in any base
+	 * @param base The base in which the input strings are
+	 * @return String after dividing the numbers in base
+	 */
 	public String divide(String str1, String str2, int base)
 	{
+		if (base > 36)
+			throw new AssertionError("The base cannot be greater than 36");
 		int num1 = baseToDecimal(str1, base);
 		int num2 = baseToDecimal(str2, base);
+		if (num2 == 0)
+			throw new AssertionError("Denominator cannot be zero");
 		return decimalToBase(num1 / num2, base);
 	}
 	
-	
+	/**
+	 * This method return the boolean after comparing the numbers for equal in bases
+	 * @param str1 The first number in any base
+	 * @param str2 The second number in any base
+	 * @return boolean after comparing the numbers in base
+	 */
 	public boolean compareForEqual(String str1, String str2)
 	{
 		if ((str1.length() == str2.length()) && (str1.compareTo(str2) == 0))
@@ -84,9 +143,15 @@ public class BaseCalc {
 		}
 	}
 	
+	/**
+	 * This method return the boolean after comparing the numbers for lesser in bases
+	 * @param str1 The first number in any base
+	 * @param str2 The second number in any base
+	 * @return boolean after comparing for lesser the numbers in base
+	 */
 	public boolean compareForLessThan(String str1, String str2)
-	{
-		if ((str1.length() < str2.length()) || (str1.compareTo(str2) < 0))
+	{                                                           
+		if ((str1.length() <= str2.length()) && (str1.compareTo(str2) < 0))
 		{
 			return true;
 		}
@@ -96,9 +161,15 @@ public class BaseCalc {
 		}
 	}
 	
-	public boolean compareForGreaterThan(String str1, String str2)
+	/**
+	 * This method return boolean if the number 1 is greater than number 2
+	 * @param str1 The first number in any base
+	 * @param str2 The second number in any base
+	 * @return boolean if number 1 is greater than number 2
+	 */
+	public boolean compareForGreaterThan(String str1, String str2)  
 	{
-		if ((str1.length() > str2.length()) || (str1.compareTo(str2) > 0))
+		if ((str1.length() >= str2.length()) && (str1.compareTo(str2) > 0))
 		{
 			return true;
 		}
@@ -107,24 +178,4 @@ public class BaseCalc {
 			return false;
 		}
 	}
-	
-	public static void main(String[] args)
-	{
-		BaseCalc obj = new BaseCalc();
-		System.out.println("0x" + obj.decimalToBase(0,16));
-		System.out.println("0x" + obj.decimalToBase(5,16));
-		System.out.println(obj.baseToDecimal("0x1F",16));
-		System.out.println(obj.baseToDecimal("0xA5D",18));
-		System.out.println(obj.baseToDecimal("0x637",8));
-		System.out.println(obj.baseToDecimal("0xB59C",25));
-		System.out.println(obj.baseToDecimal("0x0",16));
-		System.out.println(obj.add("01C","378",16));
-		System.out.println(obj.add("1B","AD",16));
-		System.out.println(obj.subtract("4A6","1B3",16));
-		System.out.println(obj.subtract("4F6","1C3",16));
-		System.out.println(obj.divide("4A6","1B3",16));
-		System.out.println(obj.multiply("4A6","1B3",16));
-		System.out.println(obj.compareForGreaterThan("1", "A"));
-	}
-	
 }
